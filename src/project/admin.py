d81982d79554d9cb32f00191ac8f609ce02873af
Vadim <vadim@kravciuk.com>
2015-11-09 19:11:34 +0200

    structure fix

--- src/project/admin.py
+++ src/project/admin.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Vadim Kravciuk, vadim@kravciuk.com'
 
-from django.db import models
+from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
