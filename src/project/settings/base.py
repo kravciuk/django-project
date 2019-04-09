8111a06c1a76995469592c766b87d9dd6551c7cc
Vadim Kravciuk <vadim@kravciuk.com>
2019-04-10 02:40:25 +0300

    Fixes

--- src/project/settings/base.py
+++ src/project/settings/base.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = (
     'vcms.comments',
 
     'project',
-    'example',
+    # 'example',
 )
 
 MIDDLEWARE = [
--- src/project/urls.py
+++ src/project/urls.py
@@ -35,4 +35,7 @@ urlpatterns = [
 
 urlpatterns += i18n_patterns(
     url(r'^i18n/', include('django.conf.urls.i18n')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
+)
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
