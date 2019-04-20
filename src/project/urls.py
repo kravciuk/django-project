33608f00b968447b24cb42e23084cd6d6077a1e5
Vadim Kravciuk <vadim@kravciuk.com>
2019-04-21 00:52:51 +0300

    Fixes

--- src/project/urls.py
+++ src/project/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib.sitemaps import views as sitemap_views
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 from sitetree.sitetreeapp import register_i18n_trees
@@ -24,22 +25,21 @@ sitemaps = {
 urlpatterns = [
     url(r'^su/', admin.site.urls),
 
-    # url(r'example/', include('apps.example.urls')),
-    # url(r'^$', views.index_page, name="index_page"),
-
     url(r'^sitemap\.xml$', sitemap_views.index, {'sitemaps': sitemaps}),
     url(r'^sitemap-(?P<section>.+)\.xml$', sitemap_views.sitemap, {'sitemaps': sitemaps}),
     url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^select2/', include('django_select2.urls')),
 
-    # url(r'^content/', include('vcms.content.urls')),
 ]
 
 urlpatterns += i18n_patterns(
+    url(r'^$',TemplateView.as_view(template_name='index.html'), name="homepage"),
+
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^share/', include('vcms.share.urls')),
     url(r'^', include('vcms.content.urls')),
+
     prefix_default_language=False
 )
 
