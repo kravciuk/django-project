fc1642afa13e8ed002e8dbb80b7b952d2d626ef1
kravciuk <vadim@kravciuk.com>
2023-04-16 21:23:42 +0300

    Refactoring

--- src/project/settings/database.py
+++ src/project/settings/database.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Vadim Kravciuk, vadim@kravciuk.com'
 
+import os
+from .base import VAR_DIR
+
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'ENGINE': 'django.db.backends.mysql',
-        "HOST": 'localhost',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        # 'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(VAR_DIR, 'local.db'),
+        # "HOST": 'localhost',
+        # 'NAME': '',
+        # 'USER': '',
+        # 'PASSWORD': '',
     }
 }
 
